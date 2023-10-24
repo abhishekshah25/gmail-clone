452d50afe1d6ce9d3eef3608d6c37f1ed270fd7b
abhishekshah25 <147745895+abhishekshah25@users.noreply.github.com>
2023-10-24 21:36:48 +0530

    Update db.js

--- gmail_server/database/db.js
+++ gmail_server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = () => {
-  const DB_URI = `mongodb://user:smudgy49@ac-5hual5z-shard-00-00.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-01.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-02.yrz8uvg.mongodb.net:27017/?ssl=true&replicaSet=atlas-w9roc2-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  const DB_URI = `mongodb://user:@ac-5hual5z-shard-00-00.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-01.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-02.yrz8uvg.mongodb.net:27017/?ssl=true&replicaSet=atlas-w9roc2-shard-0&authSource=admin&retryWrites=true&w=majority`;
   try{
     mongoose.connect(DB_URI,{useNewUrlParser: true});
     mongoose.set('strictQuery', false);
