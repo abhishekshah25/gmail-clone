c03b8333b457fff1a7cca7ff1b4889daee5dfd7e
abhishekshah25 <147745895+abhishekshah25@users.noreply.github.com>
2024-03-26 13:17:26 +0530

    Update db.js

--- gmail_server/database/db.js
+++ gmail_server/database/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const Connection = () => {
+  
   const DB_URI = `mongodb://user:@ac-5hual5z-shard-00-00.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-01.yrz8uvg.mongodb.net:27017,ac-5hual5z-shard-00-02.yrz8uvg.mongodb.net:27017/?ssl=true&replicaSet=atlas-w9roc2-shard-0&authSource=admin&retryWrites=true&w=majority`;
-  try{
+  
+  try
+  {
     mongoose.connect(DB_URI,{useNewUrlParser: true});
     mongoose.set('strictQuery', false);
     console.log('Database Connected Successfully!');
-  } catch(error){
+  } 
+  catch(error)
+  {
     console.log('Error while connecting with the database ', error.message);
   }
 }
